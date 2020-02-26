fcfa32ab380a6871e58fb803f9c5264039a60797
killallfive <869104815@qq.com>
2020-02-26 20:12:07 +0800

    mocha & istanbul

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        mochacli: {
+            options: {
+                reporter: 'spec',
+                bail: true
+            },
+            all: ['test/*.js']
+        },
+        mocha_istanbul: {
+            coverage: {
+                src: 'test'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+
+    grunt.registerTask('default', ['mochacli']);
+    grunt.registerTask('cover', ['mocha_istanbul:coverage']);
+}
