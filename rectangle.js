3b62919b43a8c0ff1b7f0e4bb855bb8e23016a88
killallfive <869104815@qq.com>
2020-02-28 13:45:19 +0800

    前端单元测试

---
+++ rectangle.js
@@ -0,0 +1,19 @@
+$(function () {
+    var $width = $('#width'),
+        $height = $('#height'),
+        $btnCal = $('#calculate'),
+        $perimeter = $('#perimeter'),
+        $area = $('#area');
+
+    $btnCal.click(function () {
+        var w = Number($width.val()),
+            h = Number($height.val());
+
+        // var p = 2 * (w + h),
+        //     a = w * h;
+        var rect = rectangle();
+
+        $perimeter.val(rect.perimeter(w, h));
+        $area.val(rect.area(w, h));
+    })
+})
